1c908fd9e8454d424edba72452056df4e754f320
Leticia Riera <leticiariera22@gmail.com>
2020-12-07 19:48:32 -0300

    Instalación react-bootstrap y NavBar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/global/NavBar';
+
+
+function App() {
+  return (
+    <NavBar />
+
+
+  );
+}
+
+export default App;
---
+++ src/components/global/NavBar.jsx
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as ReactBootStrap from "react-bootstrap";
+import '../global/Nav.css'
+
+
+
+function NavBar(){
+    return (
+    <div>
+        <ReactBootStrap.Navbar bg="light" variant="light">
+            <ReactBootStrap.Navbar.Brand href="#home">Tienda</ReactBootStrap.Navbar.Brand>
+            <ReactBootStrap.Nav className="mr-auto">
+            <ReactBootStrap.Nav.Link href="#home">Inicio</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#features">Productos</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#pricing">Ofertas</ReactBootStrap.Nav.Link>
+            </ReactBootStrap.Nav>
+            <ReactBootStrap.Form inline>
+            <ReactBootStrap.FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
+            <ReactBootStrap.Button variant="outline-primary">Buscar</ReactBootStrap.Button>
+            </ReactBootStrap.Form>
+        </ReactBootStrap.Navbar>
+    </div>
+    )
+}
+
+export default NavBar;
